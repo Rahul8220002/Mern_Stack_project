f6b8fe0e9c3b3eadf2062d824db47e810c688e8b
Rahul kumar <156082346+Rahul8220002@users.noreply.github.com>
2024-09-03 22:13:15 +0530

    Update axiosInstance.js

--- frontend/src/Components/Helpers/axiosInstance.js
+++ frontend/src/Components/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://mern-stack-project-3qfw.onrender.com";
+const BASE_URL = "https://mern-stack-project-0-3.onrender.com";
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
